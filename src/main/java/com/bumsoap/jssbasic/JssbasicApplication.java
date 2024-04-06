48e62c09f6baeb74b5ed65134a054431c5c8d44a
osparking <jbpark03@gmail.com>
2024-04-06 20:56:59 +0900

    성분 주사 패키지 명세

--- src/main/java/com/bumsoap/jssbasic/JssbasicApplication.java
+++ src/main/java/com/bumsoap/jssbasic/JssbasicApplication.java
@@ -2,8 +2,10 @@ package com.bumsoap.jssbasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.bumsoap.jssbasic.controller")
 public class JssbasicApplication {
 
 	public static void main(String[] args) {
