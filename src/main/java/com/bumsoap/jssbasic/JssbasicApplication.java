7e1cb46d2e72a664a9ccf49688495fbc538caf92
osparking <jbpark03@gmail.com>
2024-04-06 20:52:34 +0900

    최초 모습

---
+++ src/main/java/com/bumsoap/jssbasic/JssbasicApplication.java
@@ -0,0 +1,13 @@
+package com.bumsoap.jssbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JssbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JssbasicApplication.class, args);
+	}
+
+}
