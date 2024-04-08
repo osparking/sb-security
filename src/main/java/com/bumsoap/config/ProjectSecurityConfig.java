736ba321096ec63e3c61e2ecf97d62f610c6fa0f
osparking <jbpark03@gmail.com>
2024-04-08 20:39:11 +0900

    모든 요청을 거부한다

--- src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
+++ src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
@@ -7,18 +7,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+//@ formatter-off
 @Configuration
 public class ProjectSecurityConfig {
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
       throws Exception {
-    http.authorizeHttpRequests(requests -> requests
-        .requestMatchers("/myCards", "/myAccount", "/myBalance", "/myLoans")
-        .authenticated()
-        .requestMatchers("/notices", "/contact")
-        .permitAll())
+    http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
         .formLogin(withDefaults())
         .httpBasic(withDefaults());
     return http.build();
+//    http.authorizeHttpRequests(requests -> requests
+//        .requestMatchers("/myCards", "/myAccount", "/myBalance", "/myLoans")
+//        .authenticated()
+//        .requestMatchers("/notices", "/contact")
+//        .permitAll())
+//    .formLogin(withDefaults())
+//    .httpBasic(withDefaults());
+//    return http.build();
   }
+//@ formatter-on
 }
