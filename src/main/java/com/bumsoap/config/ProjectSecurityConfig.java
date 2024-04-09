9c8236521d3b749532b28d24d181d682caaa92ef
osparking <jbpark03@gmail.com>
2024-04-09 11:02:53 +0900

    메모리 사용자 두명 생성, app 성질에 정의한 한 사용자 제거

--- src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
+++ src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
@@ -5,10 +5,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
+
+  @Bean
+  InMemoryUserDetailsManager userDetailsService() {
+    UserDetails admin = User.withDefaultPasswordEncoder()
+        .username("admin")
+        .password("1111")
+        .authorities("admin")
+        .build();
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username("user")
+        .password("1111")
+        .authorities("read")
+        .build();
+    return new InMemoryUserDetailsManager(admin, user);
+  }
+
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
       throws Exception {
