376acac342a46e8f55278305c33222e0ec64945e
osparking <jbpark03@gmail.com>
2024-04-11 16:19:46 +0900

    맞춤형 사용자 적재 - 서비스 메소드

--- src/main/java/com/bumsoap/config/EasyBankUserService.java
+++ src/main/java/com/bumsoap/config/EasyBankUserService.java
@@ -1,10 +1,17 @@
 package com.bumsoap.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.bumsoap.model.Customer;
 import com.bumsoap.repository.CustomerRepository;
 
 public class EasyBankUserService implements UserDetailsService {
@@ -15,7 +22,18 @@ public class EasyBankUserService implements UserDetailsService {
   @Override
   public UserDetails loadUserByUsername(String username)
       throws UsernameNotFoundException {
-    return null;
+    String password;
+    List<GrantedAuthority> authorities;
+    List<Customer> customer = customerRepository.findByEmail(username);
+
+    if (customer.size() == 0) {
+      throw new UsernameNotFoundException("사용자로 등록되지 않은 이메일: " + username);
+    } else {
+      password = customer.get(0).getPwd();
+      authorities = new ArrayList<>();
+      authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+    }
+    return new User(username, password, authorities);
   }
 
 }
