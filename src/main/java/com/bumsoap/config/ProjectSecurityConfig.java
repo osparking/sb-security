a7a7c685c1b104c47c12c1512dfaa2e03abb13c9
osparking <jbpark03@gmail.com>
2024-04-11 09:22:43 +0900

    Jdbc 방식의 UserDetailsService 빈

--- src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
+++ src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
@@ -2,33 +2,24 @@ package com.bumsoap.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
 
-  @Bean
-  InMemoryUserDetailsManager userDetailsService() {
-    UserDetails admin = User.withUsername("admin")
-        .password("1111")
-        .authorities("admin")
-        .build();
-    UserDetails user = User.withUsername("user")
-        .password("1111")
-        .authorities("read")
-        .build();
-    return new InMemoryUserDetailsManager(admin, user);
+  public UserDetailsService userDetailsService(DataSource dataSource) {
+    return new JdbcUserDetailsManager(dataSource);
   }
 
-  @Bean
   public PasswordEncoder passwordEncoder() {
     return NoOpPasswordEncoder.getInstance();
   }
