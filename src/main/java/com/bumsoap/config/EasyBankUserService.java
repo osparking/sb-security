c623c824e0814775e3c47dff966e3b294d2f4c35
osparking <jbpark03@gmail.com>
2024-04-11 15:37:16 +0900

    서비스의 필요 요소 -  저장소 멤버

--- src/main/java/com/bumsoap/config/EasyBankUserService.java
+++ src/main/java/com/bumsoap/config/EasyBankUserService.java
@@ -1,10 +1,16 @@
 package com.bumsoap.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.bumsoap.repository.CustomerRepository;
+
 public class EasyBankUserService implements UserDetailsService {
+
+  @Autowired
+  private CustomerRepository customerRepository;
 
   @Override
   public UserDetails loadUserByUsername(String username)
