abda23d893afd4ba41f61a5e547a56088df668b0
osparking <jbpark03@gmail.com>
2024-04-14 10:29:01 +0900

    비밀번호 Bcrypt 해슁으로 암호화

--- src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
+++ src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
@@ -5,6 +5,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +15,7 @@ public class ProjectSecurityConfig {
 
   @Bean
   PasswordEncoder passwordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
+    return new BCryptPasswordEncoder();
   }
 
   @Bean
