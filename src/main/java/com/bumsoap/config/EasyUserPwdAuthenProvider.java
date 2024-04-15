726c382d1d1788e0fa2c37a8ce1fa6247c1ebda8
osparking <jbpark03@gmail.com>
2024-04-15 10:33:33 +0900

    발견된 고객 정보가 없으면 예외를 던진다.

--- src/main/java/com/bumsoap/config/EasyUserPwdAuthenProvider.java
+++ src/main/java/com/bumsoap/config/EasyUserPwdAuthenProvider.java
@@ -2,6 +2,7 @@ package com.bumsoap.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -23,7 +24,12 @@ public class EasyUserPwdAuthenProvider implements AuthenticationProvider {
     String username = authentication.getName();
     String password = authentication.getCredentials().toString();
     var customers = customerRepository.findByEmail(username);
-    return null;
+
+    if (customers.size() > 0) {
+      return null;
+    } else {
+      throw new BadCredentialsException("입력한 정보에 해당하는 고객은 없습니다.");
+    }
   }
 
   @Override
