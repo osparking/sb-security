a914a32f2d2db1a158bd8c039087eefe3c6c1104
osparking <jbpark03@gmail.com>
2024-04-15 18:19:43 +0900

    customer 테이블을 만들고 한 행을 추가한다.

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -43,3 +43,17 @@ insert into customer (email, pwd, role)
 drop table `authorities`;
 drop table `users`;
 drop table `customer`;
+
+CREATE TABLE `customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile_number` varchar(20) NOT NULL,
+  `pwd` varchar(500) NOT NULL,
+  `role` varchar(100) NOT NULL,
+  `create_dt` date DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+);
+
+INSERT INTO `customer` (`name`,`email`,`mobile_number`, `pwd`, `role`,`create_dt`)
+ VALUES ('happy','happy@example.com','9876548337', '$2y$12$oRRbkNfwuR8ug4MlzH5FOeui.//1mkd.RsOAJMbykTSupVy.x/vb2', 'admin',CURDATE());
