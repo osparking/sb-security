46230c8ee11b098b98334ea153a0284aaee5e9bc
osparking <jbpark03@gmail.com>
2024-04-15 18:27:17 +0900

    accounts 테이블을 만들고 한 행을 추가한다.

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -57,3 +57,20 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`name`,`email`,`mobile_number`, `pwd`, `role`,`create_dt`)
  VALUES ('happy','happy@example.com','9876548337', '$2y$12$oRRbkNfwuR8ug4MlzH5FOeui.//1mkd.RsOAJMbykTSupVy.x/vb2', 'admin',CURDATE());
+
+CREATE TABLE `accounts` (
+  `customer_id` int NOT NULL,
+   `account_number` int NOT NULL,
+  `account_type` varchar(100) NOT NULL,
+  `branch_address` varchar(200) NOT NULL,
+  `create_dt` date DEFAULT NULL,
+  PRIMARY KEY (`account_number`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`customer_id`) 
+    REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
+);
+
+INSERT INTO `accounts` (`customer_id`, `account_number`, `account_type`, 
+  `branch_address`, `create_dt`)
+ VALUES (1, 1865764534, 'Savings', '서울 강남구 강남대로 125번길 101호', CURDATE());
+ 
