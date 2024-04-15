9f1a8119a8822adedb9e872222a05ddbe4207326
osparking <jbpark03@gmail.com>
2024-04-15 18:53:35 +0900

    cards 테이블을 생성하고 카드 3장을 등록

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -166,4 +166,29 @@ INSERT INTO `loans` ( `customer_id`, `start_dt`, `loan_type`, `total_loan`,
 `amount_paid`, `outstanding_amount`, `create_dt`)
  VALUES ( 1, '2018-02-14', '생활비', 10000, 3500, 6500, '2018-02-14');
  
- 
+ CREATE TABLE `cards` (
+  `card_id` int NOT NULL AUTO_INCREMENT,
+  `card_number` varchar(100) NOT NULL,
+  `customer_id` int NOT NULL,
+  `card_type` varchar(100) NOT NULL,
+  `total_limit` int NOT NULL,
+  `amount_used` int NOT NULL,
+  `available_amount` int NOT NULL,
+  `create_dt` date DEFAULT NULL,
+  PRIMARY KEY (`card_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `card_customer_ibfk_1` FOREIGN KEY (`customer_id`) 
+  REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
+);
+
+INSERT INTO `cards` (`card_number`, `customer_id`, `card_type`, `total_limit`, 
+`amount_used`, `available_amount`, `create_dt`)
+ VALUES ('4565XXXX4656', 1, 'Credit', 10000, 500, 9500, CURDATE());
+
+INSERT INTO `cards` (`card_number`, `customer_id`, `card_type`, `total_limit`, 
+`amount_used`, `available_amount`, `create_dt`)
+ VALUES ('3455XXXX8673', 1, 'Credit', 7500, 600, 6900, CURDATE());
+
+INSERT INTO `cards` (`card_number`, `customer_id`, `card_type`, `total_limit`, 
+`amount_used`, `available_amount`, `create_dt`)
+ VALUES ('2359XXXX9346', 1, 'Credit', 20000, 4000, 16000, CURDATE());
