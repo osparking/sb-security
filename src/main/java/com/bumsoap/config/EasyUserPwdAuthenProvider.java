e549619fe10de3f158b8347aadd0719956392ead
osparking <jbpark03@gmail.com>
2024-04-15 10:49:44 +0900

    비번 비교, 권한 구성, 인증토큰 반환

--- src/main/java/com/bumsoap/config/EasyUserPwdAuthenProvider.java
+++ src/main/java/com/bumsoap/config/EasyUserPwdAuthenProvider.java
@@ -1,11 +1,16 @@
 package com.bumsoap.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.bumsoap.repository.CustomerRepository;
@@ -26,7 +31,14 @@ public class EasyUserPwdAuthenProvider implements AuthenticationProvider {
     var customers = customerRepository.findByEmail(username);
 
     if (customers.size() > 0) {
-      return null;
+      if (passwordEncoder.matches(password, customers.get(0).getPwd())) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+        return new UsernamePasswordAuthenticationToken(username, password,
+            authorities);
+      } else {
+        throw new BadCredentialsException("비밀번호가 잘못되었습니다!");
+      }
     } else {
       throw new BadCredentialsException("입력한 정보에 해당하는 고객은 없습니다.");
     }
