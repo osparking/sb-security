c6e24160d0d3bf2016c9720710d0c327d5544488
osparking <jbpark03@gmail.com>
2024-04-16 05:55:57 +0900

    contact_messages 테이블 지원 모델 클래스

---
+++ src/main/java/com/bumsoap/model/Contact.java
@@ -0,0 +1,30 @@
+package com.bumsoap.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contact_messages")
+public class Contact {
+
+  @Id
+  @Column(name = "contact_id")
+  private String contactId;
+
+  @Column(name = "contact_name")
+  private String contactName;
+
+  @Column(name = "contact_email")
+  private String contactEmail;
+
+  private String subject;
+
+  private String message;
+
+  @Column(name = "create_dt")
+  private Date createDt;
+}
