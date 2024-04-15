e7d027762255d5491708224f16a54895d69e131b
osparking <jbpark03@gmail.com>
2024-04-15 09:53:08 +0900

    authenticate 용 두 멤버 변수 선언

--- src/main/java/com/bumsoap/config/EasyUserPwdAuthenProvider.java
+++ src/main/java/com/bumsoap/config/EasyUserPwdAuthenProvider.java
@@ -1,11 +1,21 @@
 package com.bumsoap.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.bumsoap.repository.CustomerRepository;
 
 public class EasyUserPwdAuthenProvider implements AuthenticationProvider {
+
+  @Autowired
+  private CustomerRepository customerRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   @Override
   public Authentication authenticate(Authentication authentication)
