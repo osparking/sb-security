d2988437992fdb46a08a791fc641b2a3c450c230
osparking <jbpark03@gmail.com>
2024-04-16 06:01:36 +0900

    고객 테이블 상응 클래스 수정

--- src/main/java/com/bumsoap/model/Customer.java
+++ src/main/java/com/bumsoap/model/Customer.java
@@ -1,5 +1,8 @@
 package com.bumsoap.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,8 +14,19 @@ import lombok.Data;
 public class Customer {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "customer_id")
   private int id;
+  private String name;
   private String email;
+
+  @Column(name = "mobile_number")
+  private String mobileNumber;
+
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   private String pwd;
   private String role;
+
+  @Column(name = "create_dt")
+  private String createDt;
+
 }
