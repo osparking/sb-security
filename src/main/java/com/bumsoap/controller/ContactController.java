98edcde8e483261b2ec2304e1a23ccfd953eaa08
osparking <jbpark03@gmail.com>
2024-04-16 12:23:51 +0900

    고객의 서비스 요청 내용 저장 처리기

--- src/main/java/com/bumsoap/controller/ContactController.java
+++ src/main/java/com/bumsoap/controller/ContactController.java
@@ -1,13 +1,32 @@
 package com.bumsoap.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.sql.Date;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bumsoap.model.Contact;
+import com.bumsoap.repository.ContactRepository;
 
 @RestController
 public class ContactController {
-  @GetMapping("/contact")
-  public String saveUserInquiryDetails() {
-    return "고객의 문의 내용이 디비에 저장됨";
+
+  @Autowired
+  private ContactRepository contactRepository;
+
+  @PostMapping("/contact")
+  public Contact saveUserInquiryDetails(@RequestBody Contact contact) {
+    contact.setContactId(getServiceReqNumber());
+    contact.setCreateDt(new Date(System.currentTimeMillis()));
+    return contactRepository.save(contact);
   }
 
+  public String getServiceReqNumber() {
+    Random random = new Random();
+    int ranNum = random.nextInt(9_9999_9999 - 9999) + 9999;
+    return "SR" + ranNum;
+  }
 }
