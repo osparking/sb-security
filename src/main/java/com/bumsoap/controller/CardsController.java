014bb267af36520beb8465c2c9c2969ca341079d
osparking <jbpark03@gmail.com>
2024-04-16 12:01:55 +0900

    카드 저장소의 findByCustomerId 사용

--- src/main/java/com/bumsoap/controller/CardsController.java
+++ src/main/java/com/bumsoap/controller/CardsController.java
@@ -1,13 +1,23 @@
 package com.bumsoap.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bumsoap.model.Cards;
+import com.bumsoap.repository.CardsRepository;
 
 @RestController
 public class CardsController {
-  @GetMapping("/myCards")
-  public String getCardDetails() {
-    return "이것은 디비에서 읽어온 금융 카드 상세";
-  }
 
+  @Autowired
+  private CardsRepository cardsRepository;
+
+  @GetMapping("/myCards")
+  public List<Cards> getCardDetails(@RequestParam int custId) {
+    return cardsRepository.findByCustomerId(custId);
+  }
 }
