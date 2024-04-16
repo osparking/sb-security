799827cfb80a64a50998953002a7dbf535d699aa
osparking <jbpark03@gmail.com>
2024-04-16 12:34:47 +0900

    고객 정보 반환하는 /user 종점 처리기

--- src/main/java/com/bumsoap/controller/LoginController.java
+++ src/main/java/com/bumsoap/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.bumsoap.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +23,14 @@ public class LoginController {
 
   @Autowired
   private PasswordEncoder passwordEncoder;
-  
+
   @PostMapping("/register")
   public ResponseEntity<String> register(@RequestBody Customer customer) {
     Customer result = null;
     ResponseEntity<String> response = null;
 
     customer.setPwd(passwordEncoder.encode(customer.getPwd()));
-    
+
     try {
       result = customerRepository.save(customer);
       if (result.getId() > 0) {
@@ -38,6 +42,17 @@ public class LoginController {
           .body("발생 오류 : " + ex.getMessage());
     }
     return response;
+  }
+
+  @GetMapping("/user")
+  public Customer getUserDetailsAfterLogin(Authentication authentication) {
+    List<Customer> customers = customerRepository
+        .findByEmail(authentication.getName());
+    if (customers.size() > 0) {
+      return customers.get(0);
+    } else {
+      return null;
+    }
   }
 
 }
