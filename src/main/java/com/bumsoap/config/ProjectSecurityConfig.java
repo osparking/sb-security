403a88115f0a54c6312bb78f1decbca90a8862d8
osparking <jbpark03@gmail.com>
2024-04-17 13:14:53 +0900

    CORS 구성 - 4200 포트 angular 프론트 허용

--- src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
+++ src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
@@ -2,12 +2,18 @@ package com.bumsoap.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -21,7 +27,22 @@ public class ProjectSecurityConfig {
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
       throws Exception {
     // @formatter:off
-    http.csrf(csrf -> csrf.disable())
+    http.cors(corsCustomizer -> corsCustomizer
+          .configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(
+                HttpServletRequest request) {
+              CorsConfiguration config = new CorsConfiguration();
+              config.setAllowedOrigins(
+                  Collections.singletonList("http://localhost:4200"));
+              config.setAllowedMethods(Collections.singletonList("*"));
+              config.setAllowCredentials(true);
+              config.setAllowedHeaders(Collections.singletonList("*"));
+              config.setMaxAge(3600L);
+              return config;
+            }
+          }))
+        .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(requests -> requests
         .requestMatchers("/notices", "/contact", "/register")
           .permitAll()
