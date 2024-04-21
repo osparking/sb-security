2abf706fbd7507ead81f6422cea6dd3dea36b7f5
osparking <jbpark03@gmail.com>
2024-04-21 18:31:41 +0900

    고객 클래스에 다중 역할(롤) 지원

--- src/main/java/com/bumsoap/model/Customer.java
+++ src/main/java/com/bumsoap/model/Customer.java
@@ -1,12 +1,17 @@
 package com.bumsoap.model;
 
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
@@ -28,5 +33,9 @@ public class Customer {
 
   @Column(name = "create_dt")
   private String createDt;
+
+  @JsonIgnore
+  @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER)
+  private Set<Authority> authorities;
 
 }
