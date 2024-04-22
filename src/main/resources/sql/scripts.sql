9bb6e2f5ea56899b082ce2ce4a45a14f83644a1d
osparking <jbpark03@gmail.com>
2024-04-22 15:58:41 +0900

    권한을 제거하고 역할을 삽입한다.

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -271,3 +271,11 @@ INSERT INTO `authorities` (`customer_id`, `name`)
 
  INSERT INTO `authorities` (`customer_id`, `name`)
    VALUES (1, 'VIEWBALANCE');
+
+DELETE FROM `authorities`;
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+  VALUES (1, 'ROLE_USER');
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+  VALUES (1, 'ROLE_ADMIN'); 
