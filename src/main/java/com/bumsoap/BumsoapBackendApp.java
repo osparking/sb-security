d6c0d8fd25e3cce0164bab97aa1414766c41c3cb
osparking <jbpark03@gmail.com>
2024-04-23 14:06:30 +0900

    security filter chain 디버깅 설정

--- src/main/java/com/bumsoap/BumsoapBackendApp.java
+++ src/main/java/com/bumsoap/BumsoapBackendApp.java
@@ -2,12 +2,14 @@ package com.bumsoap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class BumsoapBackendApp {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(BumsoapBackendApp.class, args);
-	}
+  }
 
 }
