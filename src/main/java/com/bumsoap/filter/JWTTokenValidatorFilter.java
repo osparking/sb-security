340b3dfc09cb3700a1c1cee2fe447847cda02cf0
osparking <jbpark03@gmail.com>
2024-04-26 12:28:10 +0900

    JWT 에서 payload/body 까지 추출

--- src/main/java/com/bumsoap/filter/JWTTokenValidatorFilter.java
+++ src/main/java/com/bumsoap/filter/JWTTokenValidatorFilter.java
@@ -1,9 +1,18 @@
 package com.bumsoap.filter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import javax.crypto.SecretKey;
+
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.bumsoap.constants.SecurityConstants;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,8 +24,20 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request,
       HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    // TODO Auto-generated method stub
+    String jwt = request.getHeader(SecurityConstants.JWT_HEADER);
+    if (null != jwt) {
+      try {
+        SecretKey key = Keys.hmacShaKeyFor(
+            SecurityConstants.JWT_KEY.getBytes(StandardCharsets.UTF_8));
 
+        Claims claims = Jwts.parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(jwt)
+            .getPayload();
+      } catch (Exception e) {
+        throw new BadCredentialsException("Invalid Token received!");
+      }
+    }
   }
-
 }
