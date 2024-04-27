18e642e8edd4fbb3b1e114b0f4c0f62463764d15
osparking <jbpark03@gmail.com>
2024-04-27 14:05:44 +0900

    메소드 실행전 역할 검사

--- src/main/java/com/bumsoap/repository/LoansRepository.java
+++ src/main/java/com/bumsoap/repository/LoansRepository.java
@@ -3,13 +3,15 @@ package com.bumsoap.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.bumsoap.model.Loans;
 
 @Repository
 public interface LoansRepository extends CrudRepository<Loans, Integer> {
-	
-	List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+
+  @PreAuthorize("hasRole('ROOT')")
+  List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
 
 }
