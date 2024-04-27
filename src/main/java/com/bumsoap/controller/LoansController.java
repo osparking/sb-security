67ba8aae3eda783b96619adcc1e2d1066481f7e1
osparking <jbpark03@gmail.com>
2024-04-27 15:12:43 +0900

    제어기 메소드 - postAuth

--- src/main/java/com/bumsoap/controller/LoansController.java
+++ src/main/java/com/bumsoap/controller/LoansController.java
@@ -3,6 +3,7 @@ package com.bumsoap.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class LoansController {
   @Autowired
   private LoansRepository loanRepository;
 
+  @PostAuthorize("hasRole('ROOT')")
   @GetMapping("/myLoans")
   public List<Loans> getLoanDetails(@RequestParam int id) {
     return loanRepository.findByCustomerIdOrderByStartDtDesc(id);
--- src/main/java/com/bumsoap/repository/LoansRepository.java
+++ src/main/java/com/bumsoap/repository/LoansRepository.java
@@ -3,7 +3,6 @@ package com.bumsoap.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.bumsoap.model.Loans;
@@ -11,7 +10,6 @@ import com.bumsoap.model.Loans;
 @Repository
 public interface LoansRepository extends CrudRepository<Loans, Integer> {
 
-  @PreAuthorize("hasRole('USER')")
   List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
 
 }
