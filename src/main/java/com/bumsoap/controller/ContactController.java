bc6ab3f2f56806e3a1a23390988e1499762b3220
osparking <jbpark03@gmail.com>
2024-04-27 19:03:10 +0900

    PreFilter 를 Contact 제어기 메소드에 적용

--- src/main/java/com/bumsoap/controller/ContactController.java
+++ src/main/java/com/bumsoap/controller/ContactController.java
@@ -1,9 +1,11 @@
 package com.bumsoap.controller;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +20,9 @@ public class ContactController {
   private ContactRepository contactRepository;
 
   @PostMapping("/contact")
-  public Contact saveUserInquiryDetails(@RequestBody Contact contact) {
+  @PreFilter("filterObject.contactName != 'Test'")
+  public Contact saveUserInquiryDetails(@RequestBody List<Contact> contacts) {
+    Contact contact = contacts.get(0);
     contact.setContactId(getServiceReqNumber());
     contact.setCreateDt(new Date(System.currentTimeMillis()));
     return contactRepository.save(contact);
