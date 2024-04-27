dd1d1d42e9975dd86946d93687dceaf1df136873
osparking <jbpark03@gmail.com>
2024-04-27 13:00:33 +0900

    메소드 차원 보안을 구현하기 위한 첫 설정

--- src/main/java/com/bumsoap/BumsoapBackendApp.java
+++ src/main/java/com/bumsoap/BumsoapBackendApp.java
@@ -2,8 +2,12 @@ package com.bumsoap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+//@formatter:off
 @SpringBootApplication
+@EnableMethodSecurity(
+    prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class BumsoapBackendApp {
 
   public static void main(String[] args) {
