f6d3895696f7958bc2f8417e77204c38c830f6fd
osparking <jbpark03@gmail.com>
2024-05-02 21:02:24 +0900

    맞춤 JWT 롤 목록 변환기 등록

--- src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
+++ src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -28,6 +29,10 @@ public class ProjectSecurityConfig {
       throws Exception {
     var requestHandler = new CsrfTokenRequestAttributeHandler();
     requestHandler.setCsrfRequestAttributeName("_csrf");
+
+    var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter
+        .setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
     // @formatter:off
     http.sessionManagement(ssnMgmt ->
