deefe4400fddff3f24cf21d0fe15cdc74a0532cd
osparking <jbpark03@gmail.com>
2024-05-02 21:36:38 +0900

    계좌 상세 GET 처리기 인자 id 에서 email로

--- src/main/java/com/bumsoap/controller/AccountController.java
+++ src/main/java/com/bumsoap/controller/AccountController.java
@@ -1,12 +1,16 @@
 package com.bumsoap.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bumsoap.model.Accounts;
+import com.bumsoap.model.Customer;
 import com.bumsoap.repository.AccountsRepository;
+import com.bumsoap.repository.CustomerRepository;
 
 @RestController
 public class AccountController {
@@ -14,10 +18,19 @@ public class AccountController {
   @Autowired
   private AccountsRepository accountsRepository;
 
-  @GetMapping("/myAccount")
-  public Accounts getAccountDetails(@RequestParam int id) {
-    var accounts = accountsRepository.findByCustomerId(id);
+  @Autowired
+  private CustomerRepository customerRepository;
 
-    return accounts;
+  @GetMapping("/myAccount")
+  public Accounts getAccountDetails(@RequestParam String email) {
+    List<Customer> customers = customerRepository.findByEmail(email);
+    if (customers != null && !customers.isEmpty()) {
+      Accounts accounts = accountsRepository
+          .findByCustomerId(customers.get(0).getId());
+      if (accounts != null) {
+        return accounts;
+      }
+    }
+    return null;
   }
 }
